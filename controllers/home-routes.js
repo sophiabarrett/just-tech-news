51def36116b90102ae6aadddf307ae9c34e2b27f
Sophia Barrett <sophia@yoursummit.media>
2021-12-07 16:07:45 -0700

    conditionally display login/logout buttons

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,7 +29,7 @@ router.get('/', (req, res) => {
     })
         .then(dbData => {
             const posts = dbData.map(post => post.get({ plain: true }));
-            res.render('homepage', { posts });
+            res.render('homepage', { posts, loggedIn: req.session.loggedIn });
         })
         .catch(err => {
             console.log(err);
