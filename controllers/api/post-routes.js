1332d00ad1b0b7c0e1885c66e1daf56728cb15a2
Sophia Barrett <sophia@yoursummit.media>
2021-12-07 15:05:04 -0700

    add upvote button functionality

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -89,12 +89,15 @@ router.post('/', (req, res) => {
 });
 
 router.put('/upvote', (req, res) => {
-    Post.upvote(req.body, { Vote })
-        .then(updatedPostData => res.json(updatedPostData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
+    // make sure session exists first
+    if (req.session) {
+        Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+            .then(updatedPostData => res.json(updatedPostData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    }
 });
 
 router.put('/:id', (req, res) => {
