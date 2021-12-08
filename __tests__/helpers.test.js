98cb0f42153ec974dc3d0b0b5c79163c7d276c2d
Sophia Barrett <sophia@yoursummit.media>
2021-12-08 10:36:42 -0700

    add and test format url helper

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,4 +1,4 @@
-const { format_date, format_plural } = require('../utils/helpers');
+const { format_date, format_plural, format_url } = require('../utils/helpers');
 
 test('format_date() returns a date string', () => {
     const date = new Date('2020-03-20 16:12:03');
@@ -9,4 +9,14 @@ test('format_date() returns a date string', () => {
 test('format_plural() should properly pluralize word', () => {
     expect(format_plural('tiger', 2)).toBe('tigers');
     expect(format_plural('lion', 1)).toBe('lion');
+});
+
+test('format_url() returns a simplified url string', () => {
+    const url1 = format_url('http://test.com/page/1');
+    const url2 = format_url('https://www.coolstuff.com/abcdefg');
+    const url3 = format_url('https://www.google.com?q=hello');
+
+    expect(url1).toBe('test.com');
+    expect(url2).toBe('coolstuff.com');
+    expect(url3).toBe('google.com');
 });
