b6f28b295072fd405cd67969eb6811b823b89bbb
Sophia Barrett <sophia@yoursummit.media>
2021-12-08 10:31:23 -0700

    add and test format plurals helper

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,7 +1,12 @@
-const { format_date } = require('../utils/helpers');
+const { format_date, format_plural } = require('../utils/helpers');
 
 test('format_date() returns a date string', () => {
     const date = new Date('2020-03-20 16:12:03');
 
     expect(format_date(date)).toBe('3/20/2020');
+});
+
+test('format_plural() should properly pluralize word', () => {
+    expect(format_plural('tiger', 2)).toBe('tigers');
+    expect(format_plural('lion', 1)).toBe('lion');
 });
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,12 @@
 module.exports = {
     format_date: date => {
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+    },
+    format_plural: (word, amt) => {
+        if (amt !== 1) {
+            return `${word}s`;
+        }
+        return word;
     }
 }
+
